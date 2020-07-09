8f029dc39ffa62db110de20576411bb55b85cef0
joe-mcgee <thulsmans.2994@me.com>
2020-07-08 21:44:47 -0600

    better title

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -29,7 +29,7 @@ export default {
     nestData: Object
   },
   data: () => ({
-    currentPage: require('@/assets/opening-gif.gif')
+    currentPage: require('@/assets/opening-gif-2.gif')
   }),
   created() {
     

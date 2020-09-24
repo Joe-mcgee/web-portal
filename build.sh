8492284e2dd4fe1bc17b41b3bd9dc15a8a7ef5b9
joe-mcgee <thulsmans.2994@me.com>
2020-09-24 08:21:41 -0600

    debug

--- build.sh
+++ build.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
-BASEDIR=${pwd}
+set -e
+
+WEBDIR=$(pwd)/web
+APIDIR=$(pwd)/strapi
+echo $WEBDIR
+
 git pull origin src
-cd web
+cd $WEBDIR
 npm run build
 sudo rm -rf /var/www/tbh.ninja.old
-sudo mv /var/www/tbh.ninja /var/www/tbh.ninja.old
+[ -f /var/www/tbh.ninja ] && sudo mv /var/www/tbh.ninja /var/www/tbh.ninja.old
 sudo cp -R ./dist /var/www/tbh.ninja
 sudo service nginx restart
-cd $BASEDIR
-cd strapi/
+cd $APIDIR
 npm run build
 pm2 restart strapi
